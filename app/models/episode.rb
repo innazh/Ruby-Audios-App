56cd0acc23da5bed565a398fc6fa674b87dd18bb
innazh <innazh1133@gmail.com>
2025-05-26 23:13:27 -0400

    playback memorize local storage

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -6,5 +6,3 @@ class Episode < ApplicationRecord
   validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :audio_url, presence: true, format: { with: URI::regexp(%w[http https]) }
 end
-# rails generate model Like user:references episode:references
-# rails db:migrate
